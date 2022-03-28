13215a68f000cb94b332fae57a657769e3cee0fa
zurgenius <lobanov.max1202@gmail.com>
2022-03-28 18:06:47 +0300

    created todo app template and thinked of and idea of implementation (in notebook)

---
+++ homepage/js/script.js
@@ -0,0 +1,16 @@
+// window.onscroll = () => {
+//     stickyHeader()
+// }
+// const h = document.getElementById('header')
+// const sticky =  h.offsetTop
+// console.log(sticky)
+// console.log(window.pageYOffset)
+// console.log(h)
+// const stickyHeader = () => {
+//     if(window.pageYOffset > sticky) {
+//         h.classList.add("sticky")
+//     } else {
+//         h.classList.remove("sticky")
+//     }
+
+// }
---
+++ todoapp/js/todo.js
@@ -0,0 +1,11 @@
+const input = document.getElementById('input')
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const todo = input.value
+    if (todo) {
+        
+    }
+
+})
