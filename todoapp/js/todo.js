3d820353c6b1c16a6c916ca503ded61044ae4fa2
zurgenius <lobanov.max1202@gmail.com>
2022-03-28 23:26:30 +0300

    finished 1 part of todo list (drag and drop ability tomorrow)

--- todoapp/js/todo.js
+++ todoapp/js/todo.js
@@ -1,11 +1,16 @@
 const input = document.getElementById('input')
 const form = document.getElementById('form')
+const ul = document.getElementById('todo-list')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    const todo = input.value
-    if (todo) {
-        
+    const todoText = input.value
+
+    if (todoText) {
+        const li = document.createElement("li")
+        li.innerText = todoText
+        ul.appendChild(li)
+        input.value = ''
     }
 
 })
