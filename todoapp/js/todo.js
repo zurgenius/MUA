1340bf13329664ed5719ef879ba8748f2bd4c0b1
zurgenius <lobanov.max1202@gmail.com>
2022-04-01 00:52:05 +0300

    added todo features and started working on localstorage

--- todoapp/js/todo.js
+++ todoapp/js/todo.js
@@ -8,9 +8,46 @@ form.addEventListener('submit', (e) => {
 
     if (todoText) {
         const li = document.createElement("li")
+        li.setAttribute('id', 'todo-elem')
         li.innerText = todoText
+
+        li.addEventListener('click', () => {
+            li.classList.toggle('completed')
+        })
+
+        li.addEventListener("contextmenu", (e) => {
+            e.preventDefault()
+            li.remove()
+        })
+
+        const button = document.createElement("button")
+        const i = document.createElement("i")
+        
+        button.classList.add("btn")
+        i.classList.add("ri-delete-bin-line")
+        button.appendChild(i)
+        li.appendChild(button)
+        button.addEventListener('click', () => {
+            li.remove()
+        })
+
         ul.appendChild(li)
         input.value = ''
+
+        updateLS()
     }
 
 })
+
+const updateLS = () => {
+    const todosEL = document.querySelectorAll('#todo-elem')
+
+    const todos = []
+    todosEL.forEach(todo => {
+        todos.push({
+            text: todo.innerText,
+            completed: todo.classList.contains('completed') 
+        })
+    })
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
