63075474e7eb38124af2574d2a03188e78102154
zurgenius <lobanov.max1202@gmail.com>
2022-04-01 19:43:19 +0300

    ended first part of todo list

--- todoapp/js/todo.js
+++ todoapp/js/todo.js
@@ -1,18 +1,53 @@
 const input = document.getElementById('input')
 const form = document.getElementById('form')
 const ul = document.getElementById('todo-list')
+function updateLS() {
+    const todosEL = document.querySelectorAll('#todo-elem')
+
+    const todos = []
+    todosEL.forEach(todo => {
+        todos.push({
+            text: todo.innerText,
+            completed: todo.classList.contains('completed') 
+        })
+    })
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+
+
+if (todos) {
+    todos.forEach(todo => {
+        addTodo(todo)
+    })
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    const todoText = input.value
+    addTodo()
+})
+
+function addTodo(todo) {
+    let todoText = input.value
+
+    if(todo) {
+        todoText = todo.text
+    }
 
     if (todoText) {
         const li = document.createElement("li")
         li.setAttribute('id', 'todo-elem')
+        if (todo && todo.completed) {
+            li.classList.add('completed')
+
+        }
         li.innerText = todoText
 
         li.addEventListener('click', () => {
             li.classList.toggle('completed')
+            updateLS()
         })
 
         li.addEventListener("contextmenu", (e) => {
@@ -36,18 +71,5 @@ form.addEventListener('submit', (e) => {
 
         updateLS()
     }
-
-})
-
-const updateLS = () => {
-    const todosEL = document.querySelectorAll('#todo-elem')
-
-    const todos = []
-    todosEL.forEach(todo => {
-        todos.push({
-            text: todo.innerText,
-            completed: todo.classList.contains('completed') 
-        })
-    })
-    localStorage.setItem("todos", JSON.stringify(todos))
 }
+
